6253e217ee2afe37f5b578b0aa5950bed088eee0
joegeorge022 <127773439+joegeorge022@users.noreply.github.com>
2024-09-15 17:37:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // Simple greeting alert
 window.onload = function() {
   alert("Ka-Chow! Happy Birthday Christo!");
+  
+  // Optionally, control background music (pause/resume)
+  const music = document.getElementById('bg-music');
+  // Example: music.play(); or music.pause(); can be added based on custom behavior.
 };
